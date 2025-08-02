f576967a7caf9b8c26f5e141e169ea20a65e87f6
dnwls16071 <dnwls16071@naver.com>
2025-08-02 22:56:03 +0900

    #2 [feat] Batch 기본 세팅
    - application.yml 배치 테이블 설정
    - 배치 관련 디펜던시 추가
    - 주간 회의에서 나왔던 포트 접근 이슈 반영

---
+++ techlog-batch/src/main/java/com/project/techlogbatch/config/BatchConfig.java
@@ -0,0 +1,9 @@
+package com.project.techlogbatch.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchConfig {
+
+	// 국내외 블로그 배치 수집 Job/Step 설정
+}
